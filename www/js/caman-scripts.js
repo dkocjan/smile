5baec455d999533de84972e19f0217493072c39f
Daniel Kocjan <dkocjan123@gmail.com>
2017-05-03 18:43:21 +0200

    Implement layout for mobile devices using jqm, add filters for images

--- www/js/caman-scripts.js
+++ www/js/caman-scripts.js
@@ -1,3 +1,37 @@
-Caman('#image', '../img/1.jpg', function() {
-  this.brightness(10).render()
+$(function () {
+  Caman('#image', '../img/4.jpg', function () {
+    this.render()
+  })
+  
+  let filters = $('#filters button')
+  //    originalCanvas = $('#canvas'),
+  //    photo = $('#photo');
+  
+  filters.click(function (e) {
+    
+    e.preventDefault()
+    
+    let f = $(this)
+    
+    if (f.is('.active')) {
+      // Apply filters only once
+      return false
+    }
+    
+    filters.removeClass('active')
+    f.addClass('active')
+    // Listen for clicks on the filters
+    let effect = $.trim(f[ 0 ].id)
+    
+    console.log(effect)
+    
+    Caman('#image', '../img/1.jpg', function () {
+      // If such an effect exists, use it:
+      if (effect in this) {
+        this.revert()
+        this[ effect ]()
+        this.render()
+      }
+    })
+  })
 })
